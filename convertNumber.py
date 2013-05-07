3064e3157be61804b4a38a08bfe058834513c96a
Hans-Helge Bürger <mail@hig-podcast.de>
2013-05-07 18:29:51 +1000

    Convert numbers with upto base 62
    using two new functions:
    	base62_encode()
    	base62_decode()
    http://stackoverflow.com/questions/1119722/base-62-conversion-in-python

--- convertNumber.py
+++ convertNumber.py
@@ -10,38 +10,85 @@
 import sys
 import alp
 import string
-digs = string.digits + string.lowercase
+ALPHA = string.digits + string.uppercase + string.lowercase
 
+def base62_encode(num, base, alphabet=ALPHA):
+    """Encode a number in Base X
 
-def int2base(x, base):
-  if x < 0: sign = -1
-  elif x==0: return '0'
-  else: sign = 1
-  x *= sign
-  digits = []
-  while x:
-    digits.append(digs[x % base])
-    x /= base
-  if sign < 0:
-    digits.append('-')
-  digits.reverse()
-  return ''.join(digits)
+    `num`: The number to encode
+    `base`: Base of number
+    `alphabet`: The alphabet to use for encoding
+    """
+    if (num == 0):
+        return alphabet[0]
+    arr = []
+    while num:
+        rem = num % base
+        num = num // base
+        arr.append(alphabet[rem])
+    arr.reverse()
+    return ''.join(arr)
+
+def base62_decode(string, base, alphabet=ALPHA):
+	"""Decode a Base X encoded string into the number
+
+	Arguments:
+	- `string`: The encoded string
+	- `base`: base of number
+	- `alphabet`: The alphabet to use for encoding
+	"""
+	strlen = len(string)
+	num = 0
+
+	idx = 0
+	for char in string:
+	    power = (strlen - (idx + 1))
+	    num += alphabet.index(char) * (base ** power)
+	    idx += 1
+
+	return num
 
 
 if (len(sys.argv) == 4 and sys.argv[3] != "1"):
 	# calculate integer first
-	decimal = int(sys.argv[1], int(sys.argv[2]))
+	if (int(sys.argv[2]) <= 36):
+		decimal = int(sys.argv[1], int(sys.argv[2]))
+	elif (int(sys.argv[2]) > 36 and int(sys.argv[2]) <= 62):
+		decimal = base62_decode(sys.argv[1], int(sys.argv[2]))
+	else:
+		# create dictionary to create xml from it
+		errorDic = dict(title="Ohoh, your number couldn't be converted", subtitle="make sure your base is between 2 and 62", uid="error", valid=False)
+		e = alp.Item(**errorDic)
+		alp.feedback(e)
+		sys.exit()
+
 	# create dictionary to create xml from it
 	decimalDic = dict(title=str(decimal), subtitle="Decimal", uid="decimal", valid=True, arg=str(decimal), icon="icons/decimal.png")
 	d = alp.Item(**decimalDic)
 
 	# calculate new number
-	conv = int2base(decimal, int(sys.argv[3]))
+	if (int(sys.argv[3]) >= 2 and int(sys.argv[3]) <= 62):
+		conv = base62_encode(decimal, int(sys.argv[3]))
+	else:
+		# create dictionary to create xml from it
+		errorDic = dict(title="Ohoh, your number couldn't be converted", subtitle="make sure your base is between 2 and 62", uid="error", valid=False)
+		e = alp.Item(**errorDic)
+		itemsList = [d, e]
+		alp.feedback(itemsList)
+		sys.exit()
+
 	# create dictionary to create xml from it
 	convertDic = dict(title=conv, subtitle="Number to base " + str(sys.argv[3]), uid="conv", valid=True, arg=conv)
 	c = alp.Item(**convertDic)
 
-	itemsList = [d, c]
+	if (int(sys.argv[2]) >= 36 or int(sys.argv[3]) >= 36):
+		# create dictionary to create xml from it
+		infoDic = dict(title="Case-Sensitive", subtitle="Be aware, if base is >= 36 letters are case-sensitive", uid="conv", valid=True, arg=conv)
+		i = alp.Item(**infoDic)
+		itemsList = [d, c, i]
+	else:
+		itemsList = [d, c]
+
 	alp.feedback(itemsList)
 
 else:
